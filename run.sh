0aefb038f3d95a7935114043dd3e941b577c43de
mohamed <mohamedabdelfatah2027@gmail.com>
2022-02-26 16:17:19 +0200

    the NIC responds only to TCP/IPv4

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cargo b --release
-sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/tcp-rust
-$CARGO_TARGET_DIR/release/tcp-rust &
+sudo setcap cap_net_admin=eip /home/mohamed/Rust/tcp-rust/target/release/tcp-rust
+/home/mohamed/Rust/tcp-rust/target/release/tcp-rust &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
-
